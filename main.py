6bb036109d140225d1f0e06be22dc45ed758fe52
Arthur Rodrigues <arthurr.rodrigues187@gmail.com>
2025-05-14 21:13:17 +0800

    snake is moving

--- main.py
+++ main.py
@@ -19,8 +19,9 @@ clock = pygame.time.Clock()
 
 # Snake settings
 block_size = 20
-snake_pos = [100, 50] # Initial position (y, x)
+snake_pos = [100, 50] # Initial position (y, x) head
 snake_body = [[100, 50], [80, 50], [60, 50]] # Initial body (3 blocks)
+direction = 'RIGHT'
 
 #game loop
 running = True
@@ -28,6 +29,33 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP and direction != 'DOWN':
+                direction = 'UP'
+            elif event.key == pygame.K_DOWN and direction != 'UP':
+                direction = 'DOWN'
+            elif event.key == pygame.K_LEFT and direction != 'RIGHT':
+                direction = 'LEFT'
+            elif event.key == pygame.K_RIGHT and direction != 'LEFT':
+                direction = 'RIGHT'
+
+
+
+    # Move the head
+    if direction == 'RIGHT':
+        snake_pos[0] += block_size
+    elif direction == 'LEFT':
+        snake_pos[0] -= block_size
+    elif direction == 'UP':
+        snake_pos[1] -= block_size
+    elif direction == 'down':
+        snake_pos[1] += block_size
+
+
+    # add new head to the body
+    snake_body.insert(0, list(snake_pos))
+    # remove the last block (tail) to simulate the movement
+    snake_body.pop()
 
     screen.fill(black) #fills the screen with black
 
