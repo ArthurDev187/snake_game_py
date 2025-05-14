3a7a67e7fea53fafd990d41a0424ebc76ff81c35
Arthur Rodrigues <arthurr.rodrigues187@gmail.com>
2025-05-14 20:47:34 +0800

    drawing the snake

--- main.py
+++ main.py
@@ -12,6 +12,15 @@ pygame.display.set_caption('Snake Game')
 
 #colors
 black = (0, 0, 0)
+green = (0, 255, 0)
+
+# Clock (to control the frame rate)
+clock = pygame.time.Clock()
+
+# Snake settings
+block_size = 20
+snake_pos = [100, 50] # Initial position (y, x)
+snake_body = [[100, 50], [80, 50], [60, 50]] # Initial body (3 blocks)
 
 #game loop
 running = True
@@ -21,7 +30,14 @@ while running:
             running = False
 
     screen.fill(black) #fills the screen with black
+
+    # Draw snake
+    for block in snake_body:
+        pygame.draw.rect(screen, green, pygame.Rect(block[0], block[1], block_size, block_size))
+
+
     pygame.display.update() #updates the display
+    clock.tick(10) # 10 frames per second
 
 #Quit game
 pygame.quit()
